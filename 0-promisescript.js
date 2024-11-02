2379e1a7983e75835e89abaf6e43449f03fa4464
buyekeobare <buyekeobare@gmail.com>
2024-11-02 20:36:41 +0300

    comments updated

--- 0-promisescript.js
+++ 0-promisescript.js
@@ -1,6 +1,9 @@
-/*To create a Promise, you use the new Promise constructor, which takes a function with two parameters: resolve and reject. 
-The resolve function is called when the asynchronous operation completes successfully, 
-and the reject function is called when it fails.*/
+/*
+* To create a Promise, you use the new Promise constructor, 
+* which takes a function with two parameters: resolve and reject. 
+* The resolve function is called when the asynchronous operation completes successfully, 
+* and the reject function is called when it fails.
+*/
 
 // Creating a new Promise object and assigning it to the variable myPromise
 const myPromise = new Promise((resolve, reject) => {
--- 1-promisescript.js
+++ 1-promisescript.js
@@ -1,6 +1,10 @@
-/* Create a script which has two methods that return promises 
-- One of the promises should get reolved after 6 seconds timeout and the other one after 3 seconds timeout. 
-Call the promise in such a way that the second promise is invoked after the first promise is resolved.*/
+/* 
+* Create a script which has two methods that return promises 
+* One of the promises should get reolved after 6 seconds timeout 
+* and the other one after 3 seconds timeout. 
+* Call the promise in such a way that the second promise is invoked 
+* after the first promise is resolved.
+*/
 
 let myPromise1 = new Promise((resolve,reject) => {
     setTimeout(() => {
